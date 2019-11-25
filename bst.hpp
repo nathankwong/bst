38ed99885c6c64f25d0cc0314c8ed898bbf63604
nathankwong <58153601+nathankwong@users.noreply.github.com>
2019-11-24 21:05:56 -0700

    Add files via upload
    pass

--- bst.hpp
+++ bst.hpp
@@ -11,78 +11,61 @@ class BST
     {
         root=NULL;
     }
-   // ~BST();
 
  void insert(T data)
     { 
-    Node<T>* temp;
-     // std::cout<<"INSERT=================================="<<std::endl;  
+    Node<T>* temp; 
         counter++;
-
         if(root ==NULL)
         {
             root=new Node<T>(data);
             return;
         }
-        //std::cout<<"root:"<<root->getData()<<std::endl;
         temp = move(root, data);
         if(data< temp->getData())
         {
-           // std::cout<<"SET LEFT=================================="<<std::endl;
             temp->setLeft(new Node<T>(data));
-       // std::cout<<"temp:"<<temp->getData()<<std::endl;
             return;
         }
         else 
         {
-           // std::cout<<"SET RIGHT=================================="<<std::endl;
             temp->setRight(new Node<T>(data));
-             //std::cout<<"temp:"<<temp->getData()<<std::endl;
             return;
         }
 }
 
     Node<T>* move(Node<T>* cur, T data)
     {
-        //  in<<"Found: root("<<cur->getData()<<")";
-        //  out<<"Not found: root("<<cur->getData()<<")";
-        //std::cout<<"MOVES=================================="<<std::endl;
         if(cur->getData()==data)
         {
-            // std::cout<<"cur:"<<cur->getData() <<std::endl;
-            in<<"Found: root("<<cur->getData()<<")";
-            out<<"Not found: root("<<cur->getData()<<")";
             return cur;
         }
         if(data < cur->getData()) 
         {    
-            //std::cout<<"MOVES LEFT=================================="<<std::endl;
             if(cur->getLeft()== NULL)
             {
                 return cur;
             }
             cur=cur->getLeft();
-             in<<"->left("<<cur->getData()<<")";
-             out<<"->left("<<cur->getData()<<")";
+            in<<"->left("<<cur->getData()<<")";
+            out<<"->left("<<cur->getData()<<")";
             return move(cur,data);
         }
         else
         {
-            //std::cout<<"MOVES RIGHT =================================="<<std::endl;
             if(cur->getRight()== NULL )
             {
                 return cur;
             }
             cur=cur->getRight();
-             in<<"->right("<<cur->getData()<<")";
-             out<<"->right("<<cur->getData()<<")";
+            in<<"->right("<<cur->getData()<<")";
+            out<<"->right("<<cur->getData()<<")";
             return move(cur,data);
         }
     }
     bool contains (T data)
     {
          Node<T>* temp;
-       // std::cout<<"CONTAINS=================================="<<std::endl;
         temp= root;
          temp=move(root, data);
         if(temp->getData()==data)
@@ -98,62 +81,44 @@ class BST
     }
      std::string search (T data)
      {
-       // std::cout<<"SEARCH=================================="<<std::endl;
         in.str("");
         in.clear();
-        // int found =0;
-        // int notfound=0;
+        out.str("");
+        out.clear();
         Node<T>* temp;
         temp=root;
         
-       /* if(temp->getData()==data)
+        if(temp->getData()==data)
         {
-            std::cout<<"ROOT-------------------------------"<<std::endl;
             in<<"Found: "<<"root("<<temp->getData()<<")";
-            out<<"Not found: "<<"root"<<temp->getData()<<")";
-            // found=1;
+            out<<"Not found: "<<"root"<<temp->getData()<<")"; 
         }
         else {
-        if(temp->getData()>data) //left bit
+            if(temp->getData()>data) //left bit
         {
-            std::cout<<"LEFT-------------------------------"<<std::endl;
+            in<<"Found: "<<"root("<<temp->getData()<<")";
+            out<<"Not found: "<<"root("<<temp->getData()<<")";
+            temp=move(temp,data);        
+        }
+         else//right bit
+        {
             in<<"Found: "<<"root("<<temp->getData()<<")";
             out<<"Not found: "<<"root("<<temp->getData()<<")";
             temp=move(temp,data);
-            in<<"->left("<<temp->getData()<<")";
-            out<<"->left("<<temp->getData()<<")";
-            temp=move(temp,data);
-            //found=1;
         }
-         if(temp->getData()<data) //right bit
-        {
-            std::cout<<"RIGHT-------------------------------"<<std::endl;
-            in<<"Found: "<<"root("<<temp->getData()<<")";
-            out<<"Not found: "<<"root("<<temp->getData()<<")";
-            temp=move(temp,data);
-            in<<"->right("<<temp->getData()<<")";
-            out<<"->right("<<temp->getData()<<")";
-            temp=move(temp,data);
-            //found=1;
         }
-        }*/
+
             if(temp->getData()==data)
             {
-                std::cout<<"PRINT============================="<<std::endl;
                 return in.str();
-                
             }
             else
             {
-                std::cout<<" NOT PRINT============================="<<std::endl;
-                return out.str();
-                out.clear();
-             
+                return out.str();             
             } 
     } 
     int count(void)
     {
-        //std::cout<<"COUNT=================================="<<std::endl;
         return counter;
     }
     private: 
